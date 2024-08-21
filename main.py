e4812c353aaab8381855ba6cf1349d44f1ef38fa
KingOddz <alden.swartz@comcast.net>
2024-08-21 12:04:18 -0400

    Add .gitignore & main.py that returns the word count of a text

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(count_words(text))
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def count_words(text):
+    return len(text.split())
+
+main()
